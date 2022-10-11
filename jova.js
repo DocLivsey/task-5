42611dca6f6d6986c6444c0f7783ce13e9dde9bc
DocLivsey <juv.gorbachev.03@gmail.com>
2022-10-11 23:56:30 +0300

    task-5

---
+++ jova.js
@@ -0,0 +1,16 @@
+function add_new_div(){
+    let flagExam = prompt('Перейти к проверке работы? да или нет?')
+    if(flagExam == "yes" | flagExam == "da" | flagExam == "да"){
+        const new_div = '<label class="out">Введите стоимость и количество заказа: <input type="text" name="field1">руб * <input type="text" name="field2">шт <button onclick="return multiplicOfNum();">Вывести результат</button><label style="font-weight: bold;" id="result"></label></label>';
+        let add_div = document.getElementById("input");
+        add_div.innerHTML = new_div;
+    }
+}
+
+function multiplicOfNum(){
+    let f1 = document.getElementsByName("field1");
+    let f2 = document.getElementsByName("field2");
+    let r = document.getElementById("result");
+    r.innerHTML = " Итого: " + f1[0].value * f2[0].value;
+    return false;
+}
