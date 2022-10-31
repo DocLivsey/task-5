0864ecd99945ce9a89a25ef3fa87ac0aed94bf4f
DocLivsey <juv.gorbachev.03@gmail.com>
2022-11-01 00:58:42 +0300

    kruto

--- jova.js
+++ jova.js
@@ -1,20 +1,32 @@
-function add_new_div(){
-    let flagExam = prompt('Перейти к проверке работы? да или нет?')
-    if(flagExam == "yes" | flagExam == "da" | flagExam == "да" | flagExam == "Да"){
-        const new_div = '<label class="out">Введите стоимость и количество заказа: <input type="text" name="field1">руб <input type="text" name="field2">шт <button onclick="return multiplicOfNum();">Вывести результат</button><label style="font-weight: bold;" id="result"></label></label>';
-        let add_div = document.getElementById("input");
-        add_div.innerHTML = new_div;
-    }
-}
-
 function multiplicOfNum(){
     let f1 = document.getElementsByName("field1");
     let f2 = document.getElementsByName("field2");
     let r = document.getElementById("result");
     (f1[0].value >= 0 && f2[0].value >= 0) ? r.innerHTML = " Итого: " + f1[0].value * f2[0].value :
-    r.innerHTML = ' А зачем отрицательные входные данные?';
+    r.innerHTML = ' Что-то непонятное';
     return false;
 }
+function add_new_div(){
+    let flagExam = prompt('Перейти к проверке работы? да или нет?')
+    if(flagExam == "yes" | flagExam == "da" | flagExam == "да" | flagExam == "Да"){
+      let examButton = document.getElementById("examButton");
+      examButton.style.display = "none";
+      let calcDiv = document.getElementById("calc-div");
+      calcDiv.style.display = "block";
+    }
+}
+window.addEventListener('DOMContentLoaded', function (event) {
+  console.log("DOM fully loaded and parsed");
+
+  let calcDiv = document.getElementById("calc-div");
+  calcDiv.style.display = "none";
+  let examButton = document.getElementById("examButton");
+  examButton.addEventListener("click", add_new_div);
+  let resultButton = document.getElementById("resultButton");
+  resultButton.addEventListener("click", multiplicOfNum);
+});
+
+
 
 function vova()
 {
